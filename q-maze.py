c68a7024f87f0aa70be0ab7959b09bbdc394da4b
DAMODARANE Jean-Baptiste <91695685+JeanBaptiste02@users.noreply.github.com>
2025-03-05 22:10:42 +0100

    Initialisation de la grille du labyrinthe

--- q-maze.py
+++ q-maze.py
@@ -1,9 +1,13 @@
 import tkinter as tk
+import numpy as np
 
 # Dimensions du labyrinthe
 maze_size = 10
 maze_width = 500
 maze_height = 500
+
+# Labyrinthe initialisé
+maze = np.zeros((maze_size, maze_size))  # 0 = libre
 
 # Initialisation de l'interface
 root = tk.Tk()
