55592b36b37ec3dc0881194f058bac9e1c9dc2af
DAMODARANE Jean-Baptiste <91695685+JeanBaptiste02@users.noreply.github.com>
2025-03-05 22:09:14 +0100

    Ajout d'un canevas

--- q-maze.py
+++ q-maze.py
@@ -1,8 +1,14 @@
 import tkinter as tk
 
+# Dimensions du labyrinthe
+maze_width = 500
+maze_height = 500
+
 # Initialisation de l'interface
 root = tk.Tk()
 root.title("Labyrinthe Dynamique avec Q-Learning")
+canvas = tk.Canvas(root, width=maze_width, height=maze_height)
+canvas.pack()
 
 # Lancer l'application
 root.mainloop()
