023743b4670b208bc920ebbf106e2695beae3e2c
DAMODARANE Jean-Baptiste <91695685+JeanBaptiste02@users.noreply.github.com>
2025-03-05 22:11:15 +0100

    Dessin basique du labyrinthe

--- q-maze.py
+++ q-maze.py
@@ -9,11 +9,25 @@ maze_height = 500
 # Labyrinthe initialisé
 maze = np.zeros((maze_size, maze_size))  # 0 = libre
 
+# Fonction de visualisation
+def draw_maze():
+    canvas.delete('all')
+    cell_width = maze_width // maze_size
+    cell_height = maze_height // maze_size
+    
+    for i in range(maze_size):
+        for j in range(maze_size):
+            x0, y0 = j * cell_width, i * cell_height
+            x1, y1 = (j + 1) * cell_width, (i + 1) * cell_height
+            canvas.create_rectangle(x0, y0, x1, y1, fill='white', outline='lightgray')
+
 # Initialisation de l'interface
 root = tk.Tk()
 root.title("Labyrinthe Dynamique avec Q-Learning")
 canvas = tk.Canvas(root, width=maze_width, height=maze_height)
 canvas.pack()
+
+draw_maze()  # Dessine le labyrinthe
 
 # Lancer l'application
 root.mainloop()
