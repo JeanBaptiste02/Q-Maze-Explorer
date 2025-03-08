e0b46cb1e9541cd7bbfe08f8151a71aec52c0fe7
DAMODARANE Jean-Baptiste <91695685+JeanBaptiste02@users.noreply.github.com>
2025-03-08 15:36:53 +0100

    paramètres généraux du jeu avec q learning

---
+++ src/q_maze_config.py
@@ -0,0 +1,13 @@
+# Paramètres Q-Learning
+ALPHA = 0.1
+GAMMA = 0.9
+EPSILON = 0.2
+
+# Taille du labyrinthe
+MAZE_SIZE = 10
+MAZE_WIDTH = 500
+MAZE_HEIGHT = 500
+
+# Actions possibles
+ACTIONS = ['Up', 'Right', 'Down', 'Left']
+NUM_ACTIONS = len(ACTIONS)
